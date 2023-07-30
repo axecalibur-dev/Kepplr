5782b1f72f6b3db6de3481dbf96d0719bedf7018
Jai Bhatt <jaibhattwebdev@gmail.com>
2023-07-30 23:30:03 +0530

    minor improvements

--- src/crons/daily_tasks.js
+++ src/crons/daily_tasks.js
@@ -3,7 +3,7 @@ import Utils from "../utils/utils";
 import CronValues from "../globals/constants/global_constants";
 const utils = new Utils();
 export const cronJob = cron.schedule(CronValues.Late_Night_11, () => {
-    utils.kabadiwala().then(result => console.log(`Cleaning Done ${result}`)).catch(err=>{
+    utils.kabadiwala().then(result => console.log(`Cleaning Done`)).catch(err=>{
         console.log(`Cleaning Failed ${err}`)
     });
 }, {
