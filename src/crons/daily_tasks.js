e16c204ec2eb62c51b1e7f07bc49f8c33c578029
Jai Bhatt <jaibhattwebdev@gmail.com>
2023-07-31 20:38:04 +0530

    cron moved to env

--- src/crons/daily_tasks.js
+++ src/crons/daily_tasks.js
@@ -2,7 +2,7 @@ import cron from 'node-cron'
 import Utils from "../utils/utils";
 import CronValues from "../globals/constants/global_constants";
 const utils = new Utils();
-export const cronJob = cron.schedule(CronValues.Late_Night_11, () => {
+export const cronJob = cron.schedule(process.env.CRON_JOB_DB_CLEANER, () => {
     utils.kabadiwala().then(result => console.log(`Cleaning Done`)).catch(err=>{
         console.log(`Cleaning Failed ${err}`)
     });
