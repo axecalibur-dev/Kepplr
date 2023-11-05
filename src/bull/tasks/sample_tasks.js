be58a7f8e1c29a90d3590fe06e766a5fc6aeda07
Jai Bhatt <jaibhattwebdev@gmail.com>
2023-11-05 15:02:34 +0530

    minor changes to schema building pagination in feed api and aggregration pipeline

--- src/bull/tasks/sample_tasks.js
+++ src/bull/tasks/sample_tasks.js
@@ -1,13 +1,18 @@
 import { Friends } from "../../db/schema/friendSchema";
-import { Posts } from "../../db/schema/posts";
 
 class SampleTasks {
   sample_db_population_task = async (args) => {
     for (let i = 0; i < parseInt(args[0]); i++) {
       console.log(i);
-      const newFriend = new Posts({
-        friend: "654521ae513c277e1d1384a6",
-        post_string: `Ajays_Tweet_${i}`,
+      const newFriend = new Friends({
+        firstName: `Jai_${i}`,
+        lastName: `Jai_${i}`,
+        gender: `Jai_${i}`,
+        language: `Jai_${i}`,
+        age: 20 + i,
+        email: `Jai_${i}_${Math.random()}`,
+        contacts: `Jai_${i}`,
+        password: `Jai_${i}`,
       });
 
       const current_friend = await newFriend.save();
